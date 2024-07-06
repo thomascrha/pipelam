74edfee80c51356eef43d213a7bb4a5184419ddb
Thomas Crha <thomas@9bitbyte.com>
2024-07-06 23:19:42 +1000

    added really simple implementation of a FIFO pipe

--- main.c
+++ main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <pipe_path>\n", argv[0]);
+        return EXIT_FAILURE;
+    };
+
+    char *pipe_path = argv[1];
+
+    if (access(pipe_path, F_OK) == 1) {
+        // create a named pipe if it does not exist
+        mkfifo(pipe_path, 0666);
+    }
+
+    // wait for a integer to writen to the pipe file descriptor
+    while (1) {
+        FILE *pipe_fd = fopen(pipe_path, "r");
+        if (pipe_fd == NULL) {
+            perror("fopen");
+            return EXIT_FAILURE;
+        }
+
+        int integer;
+        fscanf(pipe_fd, "%d", &integer);
+        fclose(pipe_fd);
+
+        if (integer == 0) {
+            break;
+        }
+
+        printf("Received integer: %d\n", integer);
+    }
+
+    // print the integer to the console
+
+
     printf("Hello, World!\n");
     return EXIT_SUCCESS;
 }
