2c4712680bfba6d3dd849d53c0a572fe389e59a8
Thomas Crha <thomas@9bitbyte.com>
2024-10-28 18:20:47 +1100

    feat(examples): add image display example fix(window): resolve potential memory leak with pixbuf feat(window): add image rendering functionality refactor(window): extract common window creation logic

--- examples/generate-text-with-settings-json-expression.sh
+++ examples/generate-text-with-settings-json-expression.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-JSON_STRING='{"type": "text", "expression": "Hello, World!", "settings": {"window_timeout": 200, "anchor": "top-right", "margin_right": 100, "margin_top": 100}}'
+JSON_STRING='{"type": "image", "expression": "/home/tcrha/Downloads/happy.png", "settings": {"window_timeout": 2000, "anchor": "top-right", "margin_right": 100, "margin_top": 100}}'
 
 echo $JSON_STRING
--- src/window.c
+++ src/window.c
@@ -1,7 +1,10 @@
 #include <gtk/gtk.h>
 
 #include "config.h"
+#include "gdk/gdk.h"
+#include "gio/gio.h"
 #include "glib.h"
+#include "gtk/gtkshortcut.h"
 #include "gtk4-layer-shell.h"
 #include "log.h"
 
@@ -41,19 +44,19 @@ static gboolean *bow_get_anchor(enum bow_window_anchor anchor) {
     return anchors;
 }
 
-static void bow_render_image_window(GtkApplication *app, gpointer bow_config) {
+static GtkWindow *bow_render_gtk_window(GtkApplication *app, gpointer bow_config) {
     bow_log_info("Creating window");
     bow_log_info("Received string: %s", ((struct bow_config *)bow_config)->expression);
 
     if (((struct bow_config *)bow_config)->expression == NULL) {
         bow_log_error("data is NULL");
-        return;
+        return NULL;
     }
 
     GtkWindow *gtk_window = GTK_WINDOW(gtk_application_window_new(app));
     if (gtk_window == NULL) {
         bow_log_error("gtk_application_window_new() returned NULL");
-        return;
+        return NULL;
     }
 
     // Before the window is first realized, set it up to be a layer surface
@@ -79,9 +82,34 @@ static void bow_render_image_window(GtkApplication *app, gpointer bow_config) {
         bow_log_debug("anchors[%d]: %d", i, anchors[i]);
         gtk_layer_set_anchor(gtk_window, i, anchors[i]);
     }
-    //
-    // Set up a widget
-    GtkWidget *image = gtk_image_new_from_file(((struct bow_config *)bow_config)->expression);
+
+	return gtk_window;
+
+}
+
+static void bow_render_image_window(GtkApplication *app, gpointer bow_config) {
+	GtkWindow *gtk_window = bow_render_gtk_window(app, bow_config);
+	if (gtk_window == NULL) {
+		bow_log_error("gtk_render_gtk_window() returned NULL");
+		return;
+	}
+
+    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(((struct bow_config *)bow_config)->expression, NULL);
+    if (pixbuf == NULL) {
+        bow_log_error("gdk_pixbuf_new_from_stream() returned NULL");
+        return;
+    }
+
+    GdkPixbuf *scaled_pixbuf = gdk_pixbuf_scale_simple(pixbuf, 1000, 1000, GDK_INTERP_BILINEAR);
+    if (scaled_pixbuf == NULL) {
+        bow_log_error("gdk_pixbuf_scale_simple() returned NULL");
+        return;
+    }
+
+    GdkTexture *texture = gdk_texture_new_for_pixbuf(scaled_pixbuf);
+    GdkPaintable *paintable = GDK_PAINTABLE(texture);
+    GtkWidget *image = gtk_image_new_from_paintable(paintable);
+
     if (image == NULL) {
         bow_log_error("gtk_image_new_from_file() returned NULL");
         return;
@@ -90,49 +118,20 @@ static void bow_render_image_window(GtkApplication *app, gpointer bow_config) {
     gtk_window_set_child(gtk_window, image);
     gtk_window_present(gtk_window);
 
+    bow_log_debug("window_timeout: %d", ((struct bow_config *)bow_config)->window_timeout);
+    g_timeout_add(((struct bow_config *)bow_config)->window_timeout, (GSourceFunc)gtk_window_close, gtk_window);
+    gtk_window_present(gtk_window);
+
     g_signal_connect(gtk_window, "destroy", G_CALLBACK(gtk_window_close), NULL);
 }
 
 static void bow_render_text_window(GtkApplication *app, gpointer bow_config) {
-    bow_log_info("Creating window");
-    bow_log_info("Received string: %s", ((struct bow_config *)bow_config)->expression);
+	GtkWindow *gtk_window = bow_render_gtk_window(app, bow_config);
+	if (gtk_window == NULL) {
+		bow_log_error("gtk_render_gtk_window() returned NULL");
+		return;
+	}
 
-    if (((struct bow_config *)bow_config)->expression == NULL) {
-        bow_log_error("data is NULL");
-        return;
-    }
-
-    GtkWindow *gtk_window = GTK_WINDOW(gtk_application_window_new(app));
-    if (gtk_window == NULL) {
-        bow_log_error("gtk_application_window_new() returned NULL");
-        return;
-    }
-
-    // Before the window is first realized, set it up to be a layer surface
-    gtk_layer_init_for_window(gtk_window);
-
-    // Order below normal windows
-    gtk_layer_set_layer(gtk_window, GTK_LAYER_SHELL_LAYER_TOP);
-
-    // Push other windows out of the way
-    gtk_layer_auto_exclusive_zone_enable(gtk_window);
-
-    // Set margins
-    gtk_layer_set_margin(gtk_window, GTK_LAYER_SHELL_EDGE_LEFT, ((struct bow_config *)bow_config)->margin_left);
-    gtk_layer_set_margin(gtk_window, GTK_LAYER_SHELL_EDGE_RIGHT, ((struct bow_config *)bow_config)->margin_right);
-    gtk_layer_set_margin(gtk_window, GTK_LAYER_SHELL_EDGE_TOP, ((struct bow_config *)bow_config)->margin_top);
-    gtk_layer_set_margin(gtk_window, GTK_LAYER_SHELL_EDGE_BOTTOM, ((struct bow_config *)bow_config)->margin_bottom);
-
-    // Set anchors
-    gboolean *anchors = bow_get_anchor(((struct bow_config *)bow_config)->anchor);
-    // current anchor print out value of enum
-    // bow_log_debug("current anchor: %d", data->anchor);
-    for (int i = 0; i < 4; i++) {
-        bow_log_debug("anchors[%d]: %d", i, anchors[i]);
-        gtk_layer_set_anchor(gtk_window, i, anchors[i]);
-    }
-    //
-    // Set up a widget
     GtkWidget *label = gtk_label_new(NULL);
     if (label == NULL) {
         bow_log_error("gtk_label_new() returned NULL");
