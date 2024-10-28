bca3198dfde0eab4837d519c0cc90cf30beae322
Thomas Crha <thomas@9bitbyte.com>
2024-10-28 17:13:12 +1100

    added default example around settings

---
+++ examples/generate-text-with-settings-json-expression.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+JSON_STRING='{"type": "text", "expression": "Hello, World!", "settings": {"window_timeout": 2000}}'
+
+echo $JSON_STRING
--- src/message.c
+++ src/message.c
@@ -7,50 +7,83 @@
 #include <stdlib.h>
 #include <string.h>
 
+static enum bow_window_anchor bow_json_config_anchor_parse(struct json_string_s *value) {
+    if (0 == strcmp(value->string, "bottom-left")) {
+        return BOTTOM_LEFT;
+    } else if (0 == strcmp(value->string, "bottom-right")) {
+        return BOTTOM_RIGHT;
+    } else if (0 == strcmp(value->string, "top-left")) {
+        return TOP_LEFT;
+    } else if (0 == strcmp(value->string, "top-right")) {
+        return TOP_RIGHT;
+    } else if (0 == strcmp(value->string, "center")) {
+        return CENTER;
+    } else {
+        bow_log_error("Unknown anchor: %s", value->string);
+        return CENTER;
+    }
+}
+
 static void bow_json_config_settings_parse(struct json_object_s *object, struct bow_config *config) {
-    char *keys[] = {"window_timeout", "volume_expression", "anchor", "margin_left", "margin_right", "margin_top", "margin_bottom"};
+    char *keys[] = {"window_timeout", "anchor", "margin_left", "margin_right", "margin_top", "margin_bottom"};
 
     struct json_object_element_s *element = object->start;
     printf("bow_json_config_settings_parse\n");
     while (element != NULL) {
         struct json_string_s *name = element->name;
         struct json_value_s *value = element->value;
-        // struct json_object_s* value_object = json_value_as_object(value); // Use a different variable
-        struct json_string_s *value_string = json_value_as_string(value);
 
         for (size_t i = 0; i < sizeof(keys) / sizeof(keys[0]); i++) {
             char *key = keys[i];
-
-            printf("key: %s\n", key);
-            printf("name: %s\n", name->string);
-            printf("value: %s\n", json_value_as_number(value)->number);
             if (0 == strcmp(name->string, key)) {
                 printf("key: %s\n", key);
                 if (0 == strcmp(name->string, "window_timeout")) {
-                    config->window_timeout = atoi(json_value_as_number(value)->number);
+                    struct json_number_s *_value = json_value_as_number(value);
+                    if (_value != NULL) {
+                        printf("window_timeout: %s\n", _value->number);
+                        config->window_timeout = atoi(_value->number);
+                        printf("window_timeout: %d\n", config->window_timeout);
+                    }
+
                 } else if (0 == strcmp(name->string, "anchor")) {
-                    config->anchor = (enum bow_window_anchor)atoi(value_string->string);
+                    struct json_string_s *_value = json_value_as_string(value);
+                    if (_value != NULL) {
+                        config->anchor = bow_json_config_anchor_parse(_value);
+                    }
 
                 } else if (0 == strcmp(name->string, "margin_left")) {
-                    config->margin_left = atoi(value_string->string);
+                    struct json_number_s *_value = json_value_as_number(value);
+                    if (_value != NULL) {
+                        config->margin_left = atoi(_value->number);
+                    }
 
                 } else if (0 == strcmp(name->string, "margin_right")) {
-                    config->margin_right = atoi(value_string->string);
+                    struct json_number_s *_value = json_value_as_number(value);
+                    if (_value != NULL) {
+                        config->margin_right = atoi(_value->number);
+                    }
 
                 } else if (0 == strcmp(name->string, "margin_top")) {
-                    config->margin_top = atoi(value_string->string);
+                    struct json_number_s *_value = json_value_as_number(value);
+                    if (_value != NULL) {
+                        config->margin_top = atoi(_value->number);
+                    }
 
                 } else if (0 == strcmp(name->string, "margin_bottom")) {
-                    config->margin_bottom = atoi(value_string->string);
+                    struct json_number_s *_value = json_value_as_number(value);
+                    if (_value != NULL) {
+                        config->margin_bottom = atoi(_value->number);
+                    }
 
                 } else {
                     printf("unknown key: %s\n", name->string);
                 }
             }
-        }
 
-        element = element->next;
+            element = element->next;
+        }
     }
+    bow_log_debug("window_timeout: %d", config->window_timeout);
 }
 
 static void bow_json_config_parse(struct json_object_s *object, struct bow_config *config) {
@@ -92,7 +125,6 @@ static void bow_json_config_parse(struct json_object_s *object, struct bow_confi
                     }
 
                 } else if (0 == strcmp(name->string, "settings")) {
-                    printf("settings unsupported %s\n", (char *)value->payload);
                     bow_json_config_settings_parse(json_value_as_object(value), config);
                 } else {
                     printf("unknown key: %s\n", name->string);
